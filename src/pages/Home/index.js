5aee0aaf2866682ad20fbdb782908ff4e06af4bf
Rafael Korz <rafael.korz@gmail.com>
2020-03-05 23:24:33 -0300

    Listagem dos produtos e configuração do carrinho

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
+import api from '../../services/api';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
-export default function Home() {
-  return <h1>Home</h1>;
+export default class Home extends Component {
+  state = {
+    produts: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('products');
+
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({ produts: data });
+  }
+
+  render() {
+    const { produts } = this.state;
+
+    return (
+      <ProductList>
+        {produts.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#FFF" /> 3
+              </div>
+
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
